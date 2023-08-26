bd61146e4f6e055cffec79817873b885c43638c7
ahsanaaxioned <ahsana@axioned.com>
2023-08-27 01:43:36 +0530

    Added: Styling added

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 const Budget = () => {
+  const SubmitHandler = (e)=>{
+    e.preventDefault()
+  }
   return (
     <>
       <div className="basis-[41%]">
         <h1 className="common-text">BUDGET APP</h1>
-        <form
+        <form 
           action="#FIXME"
           method="post"
           className="border-2 border-[#476B3F] rounded-[4px] py-5 px-[3%] mt-6 mb-16"
+          onSubmit={SubmitHandler}
         >
           <div className="flex flex-col">
             <label htmlFor="budget" className="font-[600] ">
@@ -33,6 +37,7 @@ const Budget = () => {
           action="#FIXME"
           method="post"
           className="border-2 border-[#B62F31] rounded-[4px] py-5 px-[3%]"
+          onSubmit={SubmitHandler}
         >
           <div className="flex flex-col">
             <label htmlFor="Expense" className="font-[600] ">
@@ -69,36 +74,42 @@ const Budget = () => {
         <ul className="flex justify-between mb-14">
           <li>
             <h2 className="common-text">Budget</h2>
-            <span className="common-text text-[#476B3F]">$3000</span>
+            <span className="common-text text-[#476B3F] before:content-['\f0d6'] common-before icon">
+              $3000
+            </span>
           </li>
           <li>
             <h2 className="common-text">Expenses</h2>
-            <span className="common-text text-[#B62F31]">$3000</span>
+            <span className="common-text text-[#B62F31] before:content-['\f09d'] common-before icon">
+              $3000
+            </span>
           </li>
           <li>
             <h2 className="common-text">Balance</h2>
-            <span className="common-text text-[#476B3F]">$3000</span>
+            <span className="common-text text-[#476B3F] before:content-['\24'] common-before icon">
+              $3000
+            </span>
           </li>
         </ul>
         <ul className="flex">
-          <li className="basis-[33%] font-[600] mb-4">
+          <li className="basis-[40%] font-[600] mb-4">
             <h3>Expense Title</h3>
           </li>
-          <li className="basis-[33%] font-[600] mb-4">
+          <li className="basis-[40%] font-[600] mb-4">
             <h3>Expense Value </h3>
           </li>
         </ul>
         <ul className="flex justify-between">
-          <li>
-            <span className="text-[#B62F31] font-[600]">Coffee</span>
+          <li className="basis-[40%]">
+            <span className="text-[#B62F31] font-[700]">Coffee</span>
           </li>
-          <li>
-            <span className="text-[#B62F31] font-[600]">$200</span>
+          <li className="basis-[40%]">
+            <span className="text-[#B62F31] font-[700]">$200</span>
           </li>
-          <li>
+          <li className="basis-[5%] icon indent-[-9999px] before:indent-[0] before:content-['\f044'] cursor-pointer">
             <button>Edit</button>
           </li>
-          <li>
+          <li className="basis-[5%] icon indent-[-9999px] before:indent-0 before:content-['\f1f8'] cursor-pointer">
             <button>Delete</button>
           </li>
         </ul>
